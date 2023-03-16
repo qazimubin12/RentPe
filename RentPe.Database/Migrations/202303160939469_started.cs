26a0a0cfeab85178e62b2c39c08fc84b78f6f221
CSC19F130 Student SMIU <CSC19F130@stu.smiu.edu.pk>
2023-03-16 16:04:22 +0500

    ADdtioned

---
+++ RentPe.Database/Migrations/202303160939469_started.cs
@@ -0,0 +1,182 @@
+﻿namespace RentPe.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class started : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CustomOffers",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Owner = c.Int(nullable: false),
+                        OfferDate = c.DateTime(nullable: false),
+                        Rentee = c.Int(nullable: false),
+                        Item = c.Int(nullable: false),
+                        OfferedPrice = c.Single(nullable: false),
+                        Status = c.String(),
+                        RentingDate = c.DateTime(nullable: false),
+                        ReturnDate = c.DateTime(nullable: false),
+                        RentingPreiod = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ItemCategories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        CategoryName = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ItemImages",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ItemID = c.Int(nullable: false),
+                        ImageURL = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.RentItems",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ItemName = c.String(),
+                        UserID = c.String(),
+                        Favourites = c.Boolean(nullable: false),
+                        ItemDescription = c.String(),
+                        ItemCategory = c.String(),
+                        RentingDate = c.DateTime(nullable: false),
+                        ReturnDate = c.DateTime(nullable: false),
+                        ItemSize = c.String(),
+                        ItemColor = c.String(),
+                        Negotiable = c.Boolean(nullable: false),
+                        AvailableFrom = c.DateTime(nullable: false),
+                        AvailableTo = c.DateTime(nullable: false),
+                        Brand = c.String(),
+                        Condition = c.String(),
+                        EntryDate = c.DateTime(nullable: false),
+                        Location = c.String(),
+                        RentingPeriod = c.Int(nullable: false),
+                        Note = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.UserInfoes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Address = c.String(),
+                        Photo = c.String(),
+                        NIC = c.String(),
+                        ContactNo = c.String(),
+                        MemberSince = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Password = c.String(),
+                        Role = c.String(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.UserInfoes");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.RentItems");
+            DropTable("dbo.ItemImages");
+            DropTable("dbo.ItemCategories");
+            DropTable("dbo.CustomOffers");
+        }
+    }
+}
